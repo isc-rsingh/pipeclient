aefbb47a1663470b8010bc79175e1363959e5cf9
Raj Singh <raj.singh@intersystems.com>
2022-10-12 00:15:02 -0400

    barebones footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,8 @@
 import "./footer.css";
 export default function Footer()  {
-    return (<div className="footer-container"><span>Footer text goes here...</span></div>)
+    return (
+        <div className="footer-container">
+            <span>© 2022 InterSystems Corporation, Cambridge, MA. All Rights Reserved.</span>
+        </div>
+    )
 }
